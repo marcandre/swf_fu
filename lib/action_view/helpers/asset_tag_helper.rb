382373291104972c5ebbb53f1f8ffc5c075a8033
Marc-Andre Lafortune <github@marc-andre.ca>
2008-12-26 01:11:30 -0500

    Tested with 2.2.2

--- lib/action_view/helpers/asset_tag_helper.rb
+++ lib/action_view/helpers/asset_tag_helper.rb
@@ -29,31 +29,32 @@ module ActionView #:nodoc:
       #     image_path("logo.jpg")                         # => http://assets.example.com/images/logo.jpg
       #     swf_path("fonts/optima")                       # => http://assets.example.com/swfs/fonts/optima.swf
       def swf_path(source)
-        if defined? SwfTag  # Available since 2.1.1 (http://github.com/rails/rails/commit/900fd6eca9dd97d2341e89bcb27d7a82d62965bf )
-          SwfTag.create(self, @controller, source).public_path
+        if defined? SwfTag
+          SwfTag.new(self, @controller, source).public_path
         else
           compute_public_path(source, SwfAsset::DIRECTORY, SwfAsset::EXTENSION)
         end
       end
       alias_method :path_to_swf, :swf_path # aliased to avoid conflicts with a swf_path named route
 
-      private
-        module SwfAsset # :nodoc:
-          DIRECTORY = 'swfs'.freeze
-          EXTENSION = 'swf'.freeze
+  private
+      module SwfAsset # :nodoc:
+        DIRECTORY = 'swfs'.freeze
+        EXTENSION = 'swf'.freeze
 
-          def directory
-            DIRECTORY
-          end
-
-          def extension
-            EXTENSION
-          end
+        def directory
+          DIRECTORY
         end
-        
-        class SwfTag < AssetTag # :nodoc:
-          include SwfAsset
-        end if defined? SwfTag
+
+        def extension
+          EXTENSION
+        end
+      end
+      
+      # AssetTag is available since 2.1.1 (http://github.com/rails/rails/commit/900fd6eca9dd97d2341e89bcb27d7a82d62965bf )
+      class SwfTag < AssetTag # :nodoc:
+        include SwfAsset
+      end if defined? AssetTag 
     end
   end
 end
--- lib/swf_fu.rb
+++ lib/swf_fu.rb
@@ -1,7 +1,8 @@
 require File.dirname(__FILE__) + "/action_view/helpers/swf_fu_helper"
 require File.dirname(__FILE__) + "/action_view/helpers/asset_tag_helper"
 
-[ActionView::Helpers::AssetTagHelper, ActionView::Base].each {|mod| mod.class_eval do
+# ActionView::Helpers is for recent rails version, ActionView::Base for older ones (in which case ActionView::Helpers::AssetTagHelper is also needed for tests...)
+[ActionView::Helpers::AssetTagHelper, ActionView::Base, ActionView::Helpers].each {|mod| mod.class_eval do
   include ActionView::Helpers::SwfFuHelper
 end}
 
