fbc4ea97c5fd1d519ae2441b6a9b16d2daa3c285
Marc-Andre Lafortune <github@marc-andre.ca>
2012-08-04 10:44:26 -0400

    Move assets into app/assets and make dummy runnable

--- test/dummy/config/initializers/secret_token.rb
+++ test/dummy/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '5305be1203e464fac69468d213dbae00c025ab0b78fce0560e5a988dba12d7248c5119983d0bcfd91f81dfb11eeab7e3d6201924beef4f24b823858f7c7c9184'
+Dummy::Application.config.secret_token = 'e2fa07bf03885f6437ee9d3705a5e478db7448ee013ca8344cbe8c2279debb920ec022af3470ed1d7e4964af433bf3317428eaf8a91d4ab102e782d9cd78a47d'
